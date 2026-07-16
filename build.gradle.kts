ae4f8be984ff0272412f698fdb28aa03211b28a4
arouicharfeddine3-create <arouicharfeddine3@gmail.com>
2026-07-16 06:53:15 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,21 @@
-// Top-level build file
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        // إضافة classpath لـ KSP مع الإصدار المناسب
+        classpath("com.google.devtools.ksp:com.google.devtools.ksp.gradle.plugin:1.9.0-1.0.13")
+    }
+}
+
 plugins {
-    id("com.android.application") version "7.4.2" apply false
-    id("com.android.library") version "7.4.2" apply false
+    id("com.android.application") version "8.5.0" apply false
+    id("com.android.library") version "8.5.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
 }
 
-tasks.register<Delete>("clean") {
-    delete(rootProject.layout.buildDirectory)
+task<Delete>("clean") {
+    delete(rootProject.buildDir)
 }
