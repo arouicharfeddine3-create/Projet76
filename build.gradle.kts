eafa8fc9ec8b7619909b546819a8bacfcf334256
arouicharfeddine3-create <arouicharfeddine3@gmail.com>
2026-07-16 04:41:54 +0100

    Add KSP plugin dependencies to build.gradle.kts
    Updated build.gradle.kts to include KSP plugin dependencies and improved structure.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,17 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+// Top-level build file
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        // أضف classpath لـ KSP (يجب أن يتوافق مع إصدار Kotlin)
+        classpath("com.google.devtools.ksp:com.google.devtools.ksp.gradle.plugin:1.9.0-1.0.13")
+        // أو استخدم الصيغة: classpath("com.google.devtools.ksp:ksp-gradle-plugin:1.9.0-1.0.13")
+        // تأكد من وجود الإصدار المناسب حسب إصدار Kotlin 1.9.0
+    }
+}
+
 plugins {
     id("com.android.application") version "8.1.0" apply false
     id("com.android.library") version "8.1.0" apply false
